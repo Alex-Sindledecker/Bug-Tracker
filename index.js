2ba72cb308c6f6c3d796e8916c174b5d69e0219a
Alex Sindledecker <alexsindledecker1@gmail.com>
2023-10-08 21:01:48 -0700

    Initial commit - Setup express

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render(__dirname + "/views/index.ejs");
+});
+
+app.listen(port, () => {
+    console.log("App running on port " + port);
+});
