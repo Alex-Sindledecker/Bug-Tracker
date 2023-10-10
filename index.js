362b8b3961d2b39c12cdd696f00c0480f1213242
Alex Sindledecker <alexsindledecker1@gmail.com>
2023-10-10 08:05:18 -0700

    Setup site structure - Add initial partials

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const bypassLogin = true;
 const port = 3000;
 const app = express();
 
@@ -11,7 +13,10 @@ app.use(express.static("static"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.render(__dirname + "/views/index.ejs");
+    if (bypassLogin === false)
+        res.render(__dirname + "/views/index.ejs");
+    else
+        res.render(__dirname + "/views/index.ejs", {username: "Dev123"});
 });
 
 app.listen(port, () => {
